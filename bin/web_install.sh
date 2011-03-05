46b114f868af9cead167085301b549641aac9f3e
iandennismiller@gmail.com <iandennismiller@gmail.com@3636fdb8-8b99-a969-af0b-46c54a0c5469>
2011-03-05 21:44:59 +0000

    * updating to 0.1.2 * working web_installer
    git-svn-id: http://bullseye-code.googlecode.com/svn/trunk@19 3636fdb8-8b99-a969-af0b-46c54a0c5469

--- bin/web_install.sh
+++ bin/web_install.sh
@@ -3,19 +3,36 @@
 # to use this file, run the following command:
 # curl -s http://bullseye-code.googlecode.com/svn/trunk/bin/web_install.sh | /bin/bash
 
-VER=0.1.1
-cd /tmp
-if [ -d ~/Desktop/bullseye-code ] then
-	echo "~/Desktop/bullseye-code already exists; I don't want to overwrite it"
+VER=`curl -s http://code.google.com/p/bullseye-code/downloads/list | awk \
+  '/files\/bullseye_code.*.tar.gz/ {sub(/.tar.gz\"/,""); split($2,a,"-"); print a[3]}'`
+
+echo; echo "0. Begin installation.  Autodetected current version: $VER"
+
+if [ -d ~/Desktop/bullseye-code ]; then
+	echo "Error: ~/Desktop/bullseye-code already exists."
+	echo "I don't want to overwrite it, since you might have data in it."
 	exit 1
 fi
 mkdir ~/Desktop/bullseye-code
+
+echo; echo "1. Downloading current version of bullseye-code"
 curl -s -o /tmp/bullseye.tgz http://bullseye-code.googlecode.com/files/bullseye_code-$VER.tar.gz
+
+echo; echo "2. Extracting bullseye-code into /tmp"
+cd /tmp
 tar xvfz bullseye.tgz
-rm bullseye.tgz
+
+echo; echo "3. Building bullseye-code"
 cd bullseye_code-$VER
-cp -r ./usr ~/Desktop/bullseye-code
-echo "Please enter your password to install bullseye-code"
 python setup.py build
+
+echo; echo "4. If you are asked, please enter your password to install bullseye-code"
 sudo python setup.py install
+cp -r ./usr ~/Desktop/bullseye-code
+
+echo; echo "5. Removing temporary files"
+rm /tmp/bullseye.tgz
 rm -rf /tmp/bullseye_code-$VER
+
+echo; echo "6. bullseye-code is successfully installed."
+open "http://code.google.com/p/bullseye-code/wiki/ReadMe"
