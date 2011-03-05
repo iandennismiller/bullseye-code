9663f5b9bbb70e141f56e8d96318093ede1f0caf
iandennismiller@gmail.com <iandennismiller@gmail.com@3636fdb8-8b99-a969-af0b-46c54a0c5469>
2011-03-05 21:24:48 +0000

    * tweak installer
    git-svn-id: http://bullseye-code.googlecode.com/svn/trunk@14 3636fdb8-8b99-a969-af0b-46c54a0c5469

--- bin/web_install.sh
+++ bin/web_install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # to use this file, run the following command:
-# curl -q http://bullseye-code.googlecode.com/svn/trunk/bin/web_install.sh | /bin/bash
+# curl -s http://bullseye-code.googlecode.com/svn/trunk/bin/web_install.sh | /bin/bash
 
 VER=0.1.1
 cd /tmp
 mkdir ~/Desktop/bullseye-code
-curl -o /tmp/bullseye.tgz http://bullseye-code.googlecode.com/files/bullseye_code-$VER.tar.gz
+curl -s -o /tmp/bullseye.tgz http://bullseye-code.googlecode.com/files/bullseye_code-$VER.tar.gz
 tar xvfz bullseye.tgz
+rm bullseye.tgz
 cd bullseye_code-$VER
 cp -r ./usr ~/Desktop/bullseye-code
 echo "Please enter your password to install bullseye-code"
