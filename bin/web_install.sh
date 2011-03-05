9a7330b35b805941a4b3a0110a2e23924fe6fdd1
iandennismiller@gmail.com <iandennismiller@gmail.com@3636fdb8-8b99-a969-af0b-46c54a0c5469>
2011-03-05 21:21:47 +0000

    * changing installer
    git-svn-id: http://bullseye-code.googlecode.com/svn/trunk@13 3636fdb8-8b99-a969-af0b-46c54a0c5469

--- bin/web_install.sh
+++ bin/web_install.sh
@@ -6,7 +6,7 @@
 VER=0.1.1
 cd /tmp
 mkdir ~/Desktop/bullseye-code
-curl -o /tmp/bullseye.tgz http://bullseye-code.googlecode.com/files/bullseye_code-$VER.tgz
+curl -o /tmp/bullseye.tgz http://bullseye-code.googlecode.com/files/bullseye_code-$VER.tar.gz
 tar xvfz bullseye.tgz
 cd bullseye_code-$VER
 cp -r ./usr ~/Desktop/bullseye-code
